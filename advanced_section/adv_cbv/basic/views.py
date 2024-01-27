8c10721574430317e761ca70ba899db6adda7853
Sergey-Smirnov-redshelf <sergeysmirnov@coherentsolutions.com>
2024-01-27 16:01:17 +0300

    22:Advanced Topics - Create simple CBV

--- advanced_section/adv_cbv/basic/views.py
+++ advanced_section/adv_cbv/basic/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.views.generic import View
+from django.http import HttpResponse
 
-# Create your views here.
+
+class CBVView(View):
+    def get(self, request):
+        return HttpResponse("CLASS BASED VIEWS")
