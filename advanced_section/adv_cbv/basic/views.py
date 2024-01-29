adc28e3dec456d2902be3760c5d86cb1201d9f0d
Sergey-Smirnov-redshelf <sergeysmirnov@coherentsolutions.com>
2024-01-30 01:54:04 +0300

    22:Advanced Topics - add CRUD

--- advanced_section/adv_cbv/basic/views.py
+++ advanced_section/adv_cbv/basic/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from django.views.generic import View, TemplateView, ListView, DetailView
+from django.urls import reverse_lazy
+from django.views.generic import View, TemplateView, ListView, DetailView, CreateView, UpdateView, DeleteView
 from . import models
 
 
@@ -20,4 +21,22 @@ class SchoolDetailView(DetailView):
     # school - context by default
     model = models.School
     template_name = 'basic/school_detail.html'
+
+
+class SchoolCreateView(CreateView):
+    # school_form.html waiting by default
+    fields = ('name', 'principal', 'location')
+    model = models.School
+
+
+class SchoolUpdateView(UpdateView):
+    fields = ('name', 'principal')
+    model = models.School
+
+
+class SchoolDeleteView(DeleteView):
+    # school_confirm_delete.html waiting by default
+    # school - context by default
+    model = models.School
+    success_url = reverse_lazy("basic:list")
 
