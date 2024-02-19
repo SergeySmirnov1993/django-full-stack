f872ad2ed1955937ac929583dd62f3ca5ef2f87c
Sergey-Smirnov-redshelf <sergeysmirnov@coherentsolutions.com>
2024-02-19 23:43:37 +0300

    24: add signup, login and logout

--- simple_social_clone/simplesocial/simplesocial/settings.py
+++ simple_social_clone/simplesocial/simplesocial/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'bootstrap3',
     'accounts'
 ]
 
@@ -119,6 +120,9 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_DIR = [BASE_DIR/'static']
+
+LOGIN_REDIRECT_URL = 'test'
+LOGOUT_REDIRECT_URL = 'thanks'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
